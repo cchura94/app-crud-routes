bf3ce65577f2102b22c4d66f25d150badce433bd
cristian <cchura.cpc@gmail.com>
2025-09-23 22:45:09 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/app-crud-routes/',
+  build: {
+    outDir: 'dist'
+  },
   plugins: [vue()],
   outputDir: 'app-crud-routes'
 })
